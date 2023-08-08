22a4dcd77e618916b5c57a7d324e32fc91c44a20
Azhar Azziz <azharazziz13@gmail.com>
2023-08-08 21:06:08 +0700

    Fixing Leading and Gap of Ayat Card

--- resources/views/components/ayat-card.blade.php
+++ resources/views/components/ayat-card.blade.php
@@ -1,5 +1,5 @@
-<div class="group flex flex-col justify-center p-5 bg-white">
-    <span class="font-arabic font-bold text-2xl mb-2">{{ $verse }}</span>
+<div class="group flex flex-col justify-center gap-2 p-5 bg-white">
+    <span class="font-arabic font-bold text-2xl mb-2 leading-loose">{{ $verse }}</span>
     <span class=" font-bold">{!! $transliteration !!}</span>
     <span class="text-xs text-gray-500 italic">{!! $translation !!}</span>
     <div class="mt-2">
@@ -11,7 +11,7 @@
                 <button @click.prevent="toggle(false)" class="bg-gray-200 px-2 py-1 rounded text-xs font-bold">Tutup Tafsir</button>
             </div>
             <div v-show="toggled" class="bg-gray-200 px-2 my-2 rounded">
-                <span class="text-xs text-gray-500 italic text-justify">{!! $tafsir !!}</span>
+                <span class="text-xs text-gray-500 italic text-justify leading-none">{!! $tafsir !!}</span>
             </div>
 
         </x-splade-toggle>
